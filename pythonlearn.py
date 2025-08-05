01533fbd236c447afc5c649791486184cd1c8f41
Tien Raver <tienraver@gmail.com>
2025-08-05 11:15:36 +0000

    first commit

---
+++ pythonlearn.py
@@ -0,0 +1,12 @@
+# Khai báo biến
+print("Chuong trình tính hằng đẳng thức")
+a = float(input("Nhập số a: "))
+b = float(input("Nhập số b: "))
+# Nhập thư viện cần thiết
+import math
+# Nhập lựa chọn
+print("Hãy chọn hằng đẳng thức:")
+print("1. (a+b)^2")
+print("2. (a-b)^2")
+print("3. a^2-b^2")
+# Thân chương trình
