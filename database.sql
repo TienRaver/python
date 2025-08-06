2ba050ae0ce06774aaf36066437a3656b7e5552c
Tien Raver <tienraver@gmail.com>
2025-08-06 22:30:57 +0700

    xxx

--- database.sql
+++ database.sql
@@ -1,1 +1,11 @@
-create database 
+create database university
+USE university
+CREATE TABLE student (
+    ID INT IDENTITY(1000000,1) PRIMARY KEY NOT NULL,
+    tenSV VARCHAR(100) NOT NULL,
+    ngaysinh DATE NOT NULL,
+    gioitinh VARCHAR(20) NOT NULL,
+    quequan VARCHAR(100) NOT NULL,
+    nienkhoa VARCHAR(20) NOT NULL,
+    khoa VARCHAR(100) NOT NULL,
+)
--- themSV.py
+++ themSV.py
@@ -1,16 +1,23 @@
 # Tạo class thongtinSV và thuộc tính của nó
-class thongtinSV:
-    def __init__(self,maSV,tenSV,namsinh,quequan,nienkhoa,khoa):
-        self.maSV = maSV
+class sinhvien:
+    def __init__(self,ID,tenSV,ngaysinh,gioitinh,quequan,nienkhoa,khoa):
+        self.ID = ID
         self.tenSV = tenSV
-        self.namsinh = namsinh
+        self.ngaysinh = ngaysinh
+        self.gioitinh = gioitinh
         self.quequan = quequan
         self.nienkhoa = nienkhoa
         self.khoa = khoa
-# Method
-    def themSV(self):
-        # Kết nối với SQL
-        import pyodbc
-        conn = pyodbc.connect()
-
+# Kết nối với SQL
+import pyodbc
+conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+                      'Server=DESKTOP-50M8QJP\\SQLEXPRESS;'
+                        'Database=university;'
+                        'Trusted_Connection=yes;')
+cursor = conn.cursor()
+# Xây dựng function thêm thông tin sinh viên
+def themSV(self):
+    try:
+    except Exception as e:
+        print("Lỗi kết nối cơ sở dữ liệu:", e)
 # Gán lớp và gọi function
