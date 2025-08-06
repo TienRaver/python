9e81f24aac8d06627bdcec3c17112b4256d470d1
Tien Raver <tienraver@gmail.com>
2025-08-06 05:17:05 +0000

    xxx

--- pythonlearn.py
+++ pythonlearn.py
@@ -1,7 +1,4 @@
-# Khai báo biến
 print("Chuong trình tính hằng đẳng thức")
-a = float(input("Nhập số a: "))
-b = float(input("Nhập số b: "))
 # Nhập thư viện cần thiết
 import math
 # Nhập lựa chọn
@@ -11,5 +8,25 @@ print("2. (a-b)^2")
 print("3. a^2-b^2")
 # Thân chương trình
 while True:
-    luachon = input("Nhập lựa chọn của bạn: ")
-    print("xxx")
+    luachon = int(input("Nhập lựa chọn của bạn: "))
+    a = float(input("Nhập số a: "))
+    b = float(input("Nhập số b: "))
+    if luachon == 1:
+        ketqua = math.pow(a,2)+2*a*b+math.pow(b,2)
+        print("Kết quả là: ",round(ketqua,2))
+    elif luachon == 2:
+        ketqua = math.pow(a,2)-2*a*b+math.pow(b,2)
+        print("Kết quả là: ",round(ketqua,2))
+    elif luachon == 3:
+        ketqua = (a+b)*(a-b)
+        print("Kết quả là: ",round(ketqua,2))
+    else:
+        print("Nhập sai lựa chọn")
+        break
+    luachon1 = input("Bạn có muốn tiếp tục không (Y/N): ")
+    if luachon1 == "Y" or "y":
+        continue
+    else:
+        print("Chào tạm biệt!")
+        break
+#print("Chào tạm biệt!")
