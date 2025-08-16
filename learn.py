f8830ea8b5d654bcb24bbdcf874598da564b6fbd
Tien Raver <tienraver@gmail.com>
2025-08-16 08:49:58 +0000

    xxx

--- learn.py
+++ learn.py
@@ -1,10 +1,13 @@
+# Chuong trinh choi OAN TU TI voi may tinh
+# Nhap thu vien ngau nhien
 import random
+# Tao func getchoice() va check_win()
 def getchoice():
-    computer_list = ["Rock", "Paper", "Scissor"] # Khai bao list chon cua computer
-    player_choice = input("Enter your choice (Rock/Paper/Scissor): ")
-    computer_choice = random.choice(computer_list) # May tinh lua chon ngau nhien
-    choice = {"player": player_choice, "computer": computer_choice}
-    return choice
+    computer_list = ["rock", "paper", "scissor"] # Khai bao list chon cua computer
+    computer_choice = random.choice(computer_list) # May tinh chon ngau nhien tu computer_list
+    player_choice = input("Enter your choice (rock/paper/scissor): ") # Nhap lua chon cua nguoi choi
+    choice = {"player": player_choice, "computer": computer_choice} # Gan dict choice
+    return choice # Tra ve choice
 def check_win(player,computer):
     print(f"You chose: {player}, computer chose: {computer}")
     if player == computer:
@@ -19,4 +22,11 @@ def check_win(player,computer):
             print("You lose.")
         else:
             print("You win")
-check_win("rock","paper")
+    else:
+        if computer == "paper":
+            print("You win!")
+        else:
+            print("You lose.")
+choices = getchoice() # Goi func getchoice va gan bien choices
+result = check_win(choices["player"],choices["computer"]) # Goi func check_win va truyen bien tu choices
+print(result)
